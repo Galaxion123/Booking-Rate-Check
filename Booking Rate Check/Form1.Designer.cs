8515382087fd8b715d1ea6089074690b05188894
Andrey Mazurin <an_mazurin@hotmail.com>
2021-05-31 18:49:50 +0300

    Добавьте файлы проекта.

---
+++ Booking Rate Check/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿
+namespace Booking_Rate_Check
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ExcelFile = new System.Windows.Forms.TextBox();
+            this.ExcelBrowse = new System.Windows.Forms.Button();
+            this.TXTBrowse = new System.Windows.Forms.Button();
+            this.TXTFile = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.CheckRatesButton = new System.Windows.Forms.Button();
+            this.QuitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ExcelFile
+            // 
+            this.ExcelFile.Location = new System.Drawing.Point(30, 22);
+            this.ExcelFile.Name = "ExcelFile";
+            this.ExcelFile.ReadOnly = true;
+            this.ExcelFile.Size = new System.Drawing.Size(251, 23);
+            this.ExcelFile.TabIndex = 0;
+            this.ExcelFile.Text = "Excel Report";
+            // 
+            // ExcelBrowse
+            // 
+            this.ExcelBrowse.Location = new System.Drawing.Point(302, 22);
+            this.ExcelBrowse.Name = "ExcelBrowse";
+            this.ExcelBrowse.Size = new System.Drawing.Size(100, 23);
+            this.ExcelBrowse.TabIndex = 1;
+            this.ExcelBrowse.Text = "Select Excel File";
+            this.ExcelBrowse.UseVisualStyleBackColor = true;
+            this.ExcelBrowse.Click += new System.EventHandler(this.ExcelBrowse_Click);
+            // 
+            // TXTBrowse
+            // 
+            this.TXTBrowse.Location = new System.Drawing.Point(302, 69);
+            this.TXTBrowse.Name = "TXTBrowse";
+            this.TXTBrowse.Size = new System.Drawing.Size(100, 23);
+            this.TXTBrowse.TabIndex = 3;
+            this.TXTBrowse.Text = "Select .txt file";
+            this.TXTBrowse.UseVisualStyleBackColor = true;
+            this.TXTBrowse.Click += new System.EventHandler(this.TXTBrowse_Click);
+            // 
+            // TXTFile
+            // 
+            this.TXTFile.Location = new System.Drawing.Point(30, 69);
+            this.TXTFile.Name = "TXTFile";
+            this.TXTFile.ReadOnly = true;
+            this.TXTFile.Size = new System.Drawing.Size(251, 23);
+            this.TXTFile.TabIndex = 2;
+            this.TXTFile.Text = ".txt report";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "Excel files|*.xls";
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "openFileDialog2";
+            this.openFileDialog2.Filter = "txt files|*.txt";
+            // 
+            // CheckRatesButton
+            // 
+            this.CheckRatesButton.Enabled = false;
+            this.CheckRatesButton.Location = new System.Drawing.Point(302, 118);
+            this.CheckRatesButton.Name = "CheckRatesButton";
+            this.CheckRatesButton.Size = new System.Drawing.Size(100, 23);
+            this.CheckRatesButton.TabIndex = 4;
+            this.CheckRatesButton.Text = "Check Rates";
+            this.CheckRatesButton.UseVisualStyleBackColor = true;
+            this.CheckRatesButton.Click += new System.EventHandler(this.CheckRatesButton_Click);
+            // 
+            // QuitButton
+            // 
+            this.QuitButton.Location = new System.Drawing.Point(30, 118);
+            this.QuitButton.Name = "QuitButton";
+            this.QuitButton.Size = new System.Drawing.Size(100, 23);
+            this.QuitButton.TabIndex = 5;
+            this.QuitButton.Text = "Quit";
+            this.QuitButton.UseVisualStyleBackColor = true;
+            this.QuitButton.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(436, 157);
+            this.Controls.Add(this.QuitButton);
+            this.Controls.Add(this.CheckRatesButton);
+            this.Controls.Add(this.TXTBrowse);
+            this.Controls.Add(this.TXTFile);
+            this.Controls.Add(this.ExcelBrowse);
+            this.Controls.Add(this.ExcelFile);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Booking Rate Check";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox ExcelFile;
+        private System.Windows.Forms.Button ExcelBrowse;
+        private System.Windows.Forms.Button TXTBrowse;
+        private System.Windows.Forms.TextBox TXTFile;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+        private System.Windows.Forms.Button CheckRatesButton;
+        private System.Windows.Forms.Button QuitButton;
+    }
+}
+
---
+++ Booking Rate Check/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace Booking_Rate_Check
+{
+    public partial class Form1 : Form
+    {
+        public string filexl;
+        public string filetxt;
+        public char[] separators = new char[] { '\t', ':', ';', '|', '$', '.' };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Check_filenames()
+        {
+            if (filexl != null && filetxt != null)
+            {
+                CheckRatesButton.Enabled = true;
+            }
+            else
+            {
+                CheckRatesButton.Enabled = false;
+            }
+        }
+
+        private void Parse_txt(string file_name)
+        {
+            string orates = File.ReadAllText(filetxt);
+            string[] opera_bookings = orates.Split(separators);
+            foreach (string booking in opera_bookings)
+            {
+                Console.WriteLine($"Substring: {booking}");
+            }
+        }
+
+        private void ExcelBrowse_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                filexl = openFileDialog1.FileName;
+                ExcelFile.Text = filexl;
+            }
+            Check_filenames();
+        }
+
+        private void TXTBrowse_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog2.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                filetxt = openFileDialog2.FileName;
+                TXTFile.Text = filetxt;
+            }
+            Check_filenames();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void CheckRatesButton_Click(object sender, EventArgs e)
+        {
+            Excel.Application Booking_Rates = new Excel.Application();
+            Booking_Rates.Visible = true;
+
+            Excel.Workbook Booking_rates_workbook = Booking_Rates.Workbooks.Open(filexl);
+            Excel.Sheets Booking_rates_sheet = Booking_rates_workbook.Worksheets;
+            Excel.Worksheet brates = (Excel.Worksheet)Booking_rates_sheet.get_Item("Sheet1");
+
+            Parse_txt(filetxt);
+            Booking_rates_workbook.Close(false);
+        }
+    }
+}
